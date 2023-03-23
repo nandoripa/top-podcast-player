9a3cede9ccd56ed2c5869fd1dcfe8c39156a7724
Fernando Rivas <nandoripa@gmail.com>
2023-03-23 13:12:24 +0100

    Added Strict mode

--- main.jsx
+++ main.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import React from 'react'
+import React, { StrictMode } from 'react'
 import App from './src/App'
 
 const root = createRoot(document.getElementById('app'))
-root.render(<App />)
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
